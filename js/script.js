7a90c59b6db850ebf24d105aa1c7b938bd7b83a8
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-06-27 00:51:33 -0400

    took out error message and added re-prompt

--- js/script.js
+++ js/script.js
@@ -41,8 +41,44 @@ function clickEvent() {
 
     } else {
         alert('Please only enter "casual," semi-formal" or "formal." Thank you.');
-        var error = "Error. Cannot provide suggestion."
-        document.getElementById("suggestion").innerHTML = error;
-    } 
+        
+        var eventType = prompt("Event type?");
+        var tempFahr = prompt("What is the temperature today?");
 
+        switch (eventType) {
+            case 'casual':
+                var outfit = "something comfy";
+                break;
+            case 'semi-formal':
+                var outfit = "a polo";
+                break;
+            case 'formal':
+                var outfit = "a suit";
+                break;
+        }
+    
+        if (tempFahr > -50 && tempFahr < 54) {
+            var outWear = "a coat";
+        } else if (tempFahr >= 54 && tempFahr <= 70) {
+            var outWear = "a jacket";
+        } else if (tempFahr > 70 && tempFahr < 125) {
+            var outWear = "no jacket";
+        } else { 
+            alert("Please enter a temperature between -50 and 125.");
+            var tempFahr = prompt("What is the temperature today?");
+
+            if (tempFahr > -50 && tempFahr < 54) {
+                var outWear = "a coat";
+            } else if (tempFahr >= 54 && tempFahr <= 70) {
+                var outWear = "a jacket";
+            } else if (tempFahr > 70 && tempFahr < 125) {
+                var outWear = "no jacket";
+            }
+        }
+
+        var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + outfit + " and " + outWear + ".";
+
+        document.getElementById("suggestion").innerHTML = result;
+    } 
+``
 }
