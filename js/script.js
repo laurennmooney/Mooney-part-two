74fb3001d8a2e408907a576d3e61a8b82d124db3
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-06-27 00:38:38 -0400

    Added reprompt for temperature

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 function clickEvent() {
     var eventType = prompt("Event type?");
-    // var tempFahr = prompt("What is the temperature today?");
-    
-    // if (eventType = "casual") {
-    //     var outfit = "something comfy";
-    // } else if (eventType = "semiformal") {
-    //     var outfit = "a polo";
-    // } else if (eventType = "formal") {
-    //     var outfit = "a suit";
-    // } 
-    // well for some reason, this didn't work; let's try a switch statement instead.
 
     if (eventType === 'casual' || eventType === 'semi-formal' || eventType === 'formal') {
         var tempFahr = prompt("What is the temperature today?");
@@ -26,21 +16,25 @@ function clickEvent() {
                 break;
         }
     
-        if (tempFahr < 54) {
+        if (tempFahr > -50 && tempFahr < 54) {
             var outWear = "a coat";
         } else if (tempFahr >= 54 && tempFahr <= 70) {
             var outWear = "a jacket";
-        } else if (tempFahr > 70) {
+        } else if (tempFahr > 70 && tempFahr < 125) {
             var outWear = "no jacket";
+        } else { 
+            alert("Please enter a temperature between -50 and 125.");
+            var tempFahr = prompt("What is the temperature today?");
         }
 
-        var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + outfit + " and " + outWear + "."
+        var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + outfit + " and " + outWear + ".";
 
         document.getElementById("suggestion").innerHTML = result;
-        
+
     } else {
         alert('Please only enter "casual," semi-formal" or "formal." Thank you.');
         var error = "Error. Cannot provide suggestion."
         document.getElementById("suggestion").innerHTML = error;
-    }
+    } 
+
 }
